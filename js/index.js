8f4c2f8dcd47734106e2f8d4c62df49fca0613d6
Thibault Calliauw <calliauw.t@gmail.com>
2022-12-09 23:23:52 +0100

    images added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
 import Carousel from "./carousel";
+import Carousel2 from "./carousel2";
 
-const spotlightCarousel = new Carousel(
-  document.querySelector(".carousel-container")
-);
+const spotlight = new Carousel2(document.querySelector(".carousel-container"));
+
+document.querySelector(".theme-btn").addEventListener("click", themeSwitch);
+
+function themeSwitch() {
+  if (document.querySelector(".theme-btn").classList.contains("fa-sun")) {
+    document.body.style.backgroundColor = "var(--clr-body-light)";
+    document.querySelector("header").style.backgroundColor =
+      "var(--clr-body-light)";
+    document.querySelector("header").style.color = "var(--clr-body-dark)";
+    document.querySelector(".nav-container").style.borderColor =
+      "var(--clr-body-dark)";
+    document.querySelector(".dark").classList.add("hidden");
+    document.querySelector(".light").classList.remove("hidden");
+    document.querySelector(".theme-btn").classList.remove("fa-sun");
+    document.querySelector(".theme-btn").classList.add("fa-moon");
+  } else if (
+    document.querySelector(".theme-btn").classList.contains("fa-moon")
+  ) {
+    document.body.style.backgroundColor = "var(--clr-body-dark)";
+    document.querySelector("header").style.backgroundColor =
+      "var(--clr-body-dark)";
+    document.querySelector("header").style.color = "var(--clr-body-light)";
+    document.querySelector(".nav-container").style.borderColor =
+      "var(--clr-body-light)";
+    document.querySelector(".dark").classList.remove("hidden");
+    document.querySelector(".light").classList.add("hidden");
+    document.querySelector(".theme-btn").classList.add("fa-sun");
+    document.querySelector(".theme-btn").classList.remove("fa-moon");
+  }
+}
---
+++ js/themeswitch.js
@@ -0,0 +1,27 @@
+// export function themeSwitch() {
+//   if (document.querySelector(".theme-btn").classList.contains("fa-sun")) {
+//     document.body.style.backgroundColor = "var(--clr-body-light)";
+//     document.querySelector("header").style.backgroundColor =
+//       "var(--clr-body-light)";
+//     document.querySelector("header").style.color = "var(--clr-body-dark)";
+//     document.querySelector(".nav-container").style.borderColor =
+//       "var(--clr-body-dark)";
+//     document.querySelector(".dark").classList.add("hidden");
+//     document.querySelector(".light").classList.remove("hidden");
+//     document.querySelector(".theme-btn").classList.remove("fa-sun");
+//     document.querySelector(".theme-btn").classList.add("fa-moon");
+//   } else if (
+//     document.querySelector(".theme-btn").classList.contains("fa-moon")
+//   ) {
+//     document.body.style.backgroundColor = "var(--clr-body-dark)";
+//     document.querySelector("header").style.backgroundColor =
+//       "var(--clr-body-dark)";
+//     document.querySelector("header").style.color = "var(--clr-body-light)";
+//     document.querySelector(".nav-container").style.borderColor =
+//       "var(--clr-body-light)";
+//     document.querySelector(".dark").classList.remove("hidden");
+//     document.querySelector(".light").classList.add("hidden");
+//     document.querySelector(".theme-btn").classList.add("fa-sun");
+//     document.querySelector(".theme-btn").classList.remove("fa-moon");
+//   }
+// }
