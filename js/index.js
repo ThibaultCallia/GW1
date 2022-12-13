b62ba2ed55656ded06613de594745fc4eab2aaa5
Thibault Calliauw <calliauw.t@gmail.com>
2022-12-13 09:34:18 +0100

    welcome screen placeholder

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 import Carousel2 from "./carousel2";
 import Filter from "./Filter";
 
-const spotlight = new Carousel2(document.querySelector(".carousel-container"));
+// const spotlight = new Carousel2(document.querySelector(".carousel-container"));
 const mainFilter = new Filter(document.querySelector(".products__filter"));
 
 // document.querySelector(".theme-btn").addEventListener("click", themeSwitch);
+
+// WELCOME
+// const welcomeContainer = document.querySelector(".welcome-container");
+// const left = document.querySelector(".welcome__left");
+// const moveEvent = (e) => {
+//   const welcomeWidth = welcomeContainer.clientWidth;
+//   const x = ((e.pageX - welcomeContainer.offsetLeft) / welcomeWidth) * 100;
+//   console.log(x);
+//   left.style.width = `${x}%`;
+// };
+
+// document.onmousemove = (e) => moveEvent(e);
