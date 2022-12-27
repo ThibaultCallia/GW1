43bf7bf6c1341616869b4553916c7e5f001eab1f
Rinzin Tenzin <rinzin.tenzin@protonmail.com>
2022-12-27 20:58:18 +0100

    latest changes

--- admin/admin.php
+++ admin/admin.php
@@ -2,12 +2,28 @@
 session_start();
 
 // ik check of dat sessionlogin bestaat en of die op TRUE staat
-if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
-    header("Location: ./../login.php");
-    exit;
-}
+// if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
+//     header("Location: ./../login.php");
+//     exit;
+// }
 
 ?>
 
-<h1>Hallo <?= $_SESSION["username"] ?></h1>
-<a href="./logout.php">Uitloggen</a>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Hallo <?= $_SESSION["username"] ?></h1>
+    <a href="./logout.php">Uitloggen</a>
+</body>
+
+</html>
