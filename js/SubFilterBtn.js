a32a5ed5f0bee3e6b240f463ea525ab589b1a9d2
Thibault Calliauw <calliauw.t@gmail.com>
2023-01-01 11:43:21 +0100

    Pseudo Code on SubFilter js class

--- js/SubFilterBtn.js
+++ js/SubFilterBtn.js
@@ -1,8 +1,26 @@
-class SubFilterBtn{
-    constructor(filter){
-        this.filter = filter;
-    }
-    clear()
+class SubFilterBtn {
+  constructor(filter) {
+    this.filter = filter;
+    this.generateClear();
+    this.ref = this.init();
+  }
+  init() {
+    document
+      .querySelector(".subfilter-active")
+      .insertAdjacentHTML("beforeend", `<div class = "placeholder" ></div>`);
+
+    return "look for last inserted element and return";
+  }
+  generateClear() {
+    this.ref.querySelector().addEventlistener("click", clear);
+  }
 }
 
-export default SubFilterBtn
+/* When clicking on cross icon in filter, it must remove this id from 
+GlobalFilter, launch filter function and delete himself from parent div
+--->>> how to access GlobalFilter from within class? Custom Event?
+---->>> this.GlobalFilter from Filter should be static? Can be accessed by other classes
+
+*/
+
+export default SubFilterBtn;
