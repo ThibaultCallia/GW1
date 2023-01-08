d8d5d1d23aa8bb8bd9d0eaaadd46d6e08ea1e8ad
Rinzin Tenzin <rinzin.tenzin@protonmail.com>
2023-01-08 14:43:22 +0100

    html/css admin

--- php/admin.php
+++ php/admin.php
@@ -18,6 +18,9 @@ if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./../CSS/add_product.css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&display=swap" rel="stylesheet">
     <title>Admin Page</title>
 </head>
 
@@ -25,81 +28,20 @@ if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
     <?php include './includes/nav.php'; ?>
     <main class="admin">
         <section class="welcome">
-            <h1>Hallo <?= $_SESSION["username"] ?></h1>
-            <a href="./add_products.php">Add products</a>
+            <h1>Hallo <?= $_SESSION["username"] ?>.</h1>
+            <div class="btns">
+                <a href="./#prds">View products</a>
+                <a href="./add_products.php">Add products</a>
+            </div>
         </section>
-        <section class="products">
+        <section class="products" id="prds">
             <div class="product-section-bar">
                 <p>Products</p>
             </div>
             <div class="product-sections">
-                <table id="KB" border="1" width="100%" celspacing="0">
-                    <thead>
-                        <tr>
-                            <th colspan="9">Keyboards</th>
-                        </tr>
-                        <tr>
-                            <th>Active</th>
-                            <th>id</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Brand</th>
-                            <th>Colors</th>
-                            <th>Rating</th>
-                            <th>Spotlight</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        include($_SERVER['DOCUMENT_ROOT'] . '/php/includes/admin-products.php');
-                        ?>
-                    </tbody>
-                </table>
-
-                <table id="KC" border="1" width="100%" celspacing="0">
-                    <thead>
-                        <h2 for="KC">Keycaps</h2>
-                        <tr>
-                            <th>Active</th>
-                            <th>id</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Brand</th>
-                            <th>Colors</th>
-                            <th>Rating</th>
-                            <th>Spotlight</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        include($_SERVER['DOCUMENT_ROOT'] . '/php/includes/admin-products.php');
-                        ?>
-                    </tbody>
-                </table>
-
-                <table id="SW" border="1" width="100%" celspacing="0">
-                    <thead>
-                        <h2 for="SW">Switches</h2>
-                        <tr>
-                            <th>Active</th>
-                            <th>id</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Brand</th>
-                            <th>Colors</th>
-                            <th>Rating</th>
-                            <th>Spotlight</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        include($_SERVER['DOCUMENT_ROOT'] . '/php/includes/admin-products.php');
-                        ?>
-                    </tbody>
-                </table>
+                <?php
+                include($_SERVER['DOCUMENT_ROOT'] . '/php/includes/admin-products.php');
+                ?>
             </div>
         </section>
     </main>
