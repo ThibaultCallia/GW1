10802bf5127db44d70cbe49ffd53eb456f80248a
Rinzin Tenzin <rinzin.tenzin@protonmail.com>
2023-01-08 19:07:16 +0100

    most recent admin

--- php/admin.php
+++ php/admin.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./../CSS/add_product.css" />
+    <link rel="stylesheet" href="./../CSS/admin.css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,7 +30,7 @@ if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
         <section class="welcome">
             <h1>Hallo <?= $_SESSION["username"] ?>.</h1>
             <div class="btns">
-                <a href="./#prds">View products</a>
+                <a href="#prds">View products</a>
                 <a href="./add_products.php">Add products</a>
             </div>
         </section>
