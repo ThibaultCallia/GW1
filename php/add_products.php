c80aa3bc28111dd77e439aa1e7ee9d4ee3a52144
Rinzin Tenzin <rinzin.tenzin@protonmail.com>
2023-01-09 16:52:04 +0100

    main before parcel

--- php/add_products.php
+++ php/add_products.php
@@ -28,7 +28,7 @@ $mysqli->close();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="./../CSS/add_product.css" />
+    <link rel="stylesheet" href="./../CSS/admin.css" />
     <link rel="stylesheet" href="./../CSS/select.css" />
     <link rel="stylesheet" href="./../cssMain/hidden.css" />
     <script src="https://kit.fontawesome.com/cdb38844a4.js" crossorigin="anonymous"></script>
